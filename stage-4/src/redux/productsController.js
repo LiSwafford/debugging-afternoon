a10fab620c8f1944143b20121783836883e86281
Li Swafford <liswafford@yahoo.com>
2018-05-29 23:28:12 -0500

    love debugging

--- stage-4/src/redux/productsController.js
+++ stage-4/src/redux/productsController.js
@@ -1,6 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
-const getAllProducts = function() {
-    return axios.get("https://practiceapi.devmountain.com/products/")
+export const getAllProducts = function() {
+  return axios
+    .get("https://practiceapi.devmountain.com/products/")
     .then(response => response.data);
-}
+};
+
+//why can't we use export default getAllProducts???????????
